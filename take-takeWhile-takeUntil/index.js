a9b972b5a10ae29595955602d09d71936533cb05
Igor Kucherenko <hotncold1989@gmail.com>
2018-08-19 21:36:33 +0300

    Added "take", "takeWhile", "takeUntil"

---
+++ take-takeWhile-takeUntil/index.js
@@ -0,0 +1,23 @@
+import { timer } from 'rxjs';
+import { take, takeWhile, takeUntil } from 'rxjs/operators';
+
+// It emits first 5 values.
+interval(300)
+  .pipe(
+    take(5)
+  )
+  .subscribe(data => console.log(data));
+
+// It emits values until they are less than 10.
+interval(300)
+  .pipe(
+    takeWhile(data => data < 10)
+  )
+  .subscribe(data => console.log(data));
+
+// It emits values during 2000 ms.
+interval(300)
+  .pipe(
+    takeUntil(timer(2000))
+  )
+  .subscribe(data => console.log(data));
