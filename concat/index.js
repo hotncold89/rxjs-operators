b10c7d760fa412343b2aab1564814aabb6e4ca85
Igor Kucherenko <hotncold1989@gmail.com>
2018-08-19 22:27:36 +0300

    Added explanations to "concat"

--- concat/index.js
+++ concat/index.js
@@ -1,6 +1,8 @@
 import { interval } from 'rxjs';
 import { concat, tap, take } from 'rxjs/operators';
 
+// Concat the second stream after the first one is complete.
+// In case the first stream is endless, the second one will never be concatenated.
 interval(300)
   .pipe(
     take(10),
